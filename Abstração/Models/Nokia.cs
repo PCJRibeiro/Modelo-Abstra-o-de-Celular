4a29a1a4b4bedf63ce746ea8eb256516b18c011c
PCJRibeiro <pcjr2003@gmail.com>
2024-03-06 15:20:23 -0300

    projeto de celular pronto

---
+++ Abstração/Models/Nokia.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System;
+
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base (numero,modelo,imei,memoria)
+        {
+            Numero = numero;
+          
+        }
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no nokia");
+        }
+    }
+}
---
+++ Abstração/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("1194792", "modelo 1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("3219039", "modelo 2", "22222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");

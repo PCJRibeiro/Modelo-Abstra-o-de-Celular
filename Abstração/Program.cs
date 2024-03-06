f3de5d2d65602c56480392b811db66c89d2c3cda
PCJRibeiro <pcjr2003@gmail.com>
2024-03-06 15:26:26 -0300

    conferindo todas as saidas

--- Abstração/Program.cs
+++ Abstração/Program.cs
@@ -12,3 +12,18 @@ Console.WriteLine("Smartphone Iphone: ");
 Smartphone iphone = new Iphone("3219039", "modelo 2", "22222222", 128);
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia2 = new Nokia("11930438", "modelo 3", "11111111", 32);
+nokia2.ReceberLigacao();
+nokia2.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone2 = new Iphone("786953", "modelo 4", "22222222", 254);
+iphone2.Ligar();
+iphone2.InstalarAplicativo("Facebook");
+
